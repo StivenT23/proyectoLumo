884f4e38d3840c4188c45ab682e6874e056ded5d
DavidGaitan7 <166175344+DavidGaitan7@users.noreply.github.com>
2026-03-12 08:21:05 -0500

    Primer commit proyecto LUMO
    (cherry picked from commit 5bad5a4dd3102c1969e478edf7fdde1fd8bcac8b)

---
+++ src/main/java/com/LUMO/LUMO_Proyecto/LumoProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.LUMO.LUMO_Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumoProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LUMO/LUMO_Proyecto/LumoProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LUMO.LUMO_Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
